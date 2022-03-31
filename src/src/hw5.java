ddb12246752dd35d19a09735b3b2063710618d7f
Ivan Hossain <hoivan255@gmail.com>
2022-03-30 21:59:32 -0400

    hw5

---
+++ src/src/hw5.java
@@ -0,0 +1,39 @@
+public class hw5 {
+    //Q2
+
+    ///To print length:
+    public class hw  {
+        public static void main(String[] args) {
+            String txt = "Health was Earlier said to be the ability of the body functioning WELL";
+            System.out.println("The length of the txt string is: " + txt.length());
+        }
+    }
+//Returns 70
+
+    //To print number of string:
+
+    public class hw {
+        public static void main(String[] args) {
+            String[] sentence = {"Health", "was", "Earlier", "said" , "to", "be", "the", "ability", "of", "the", "body", "functioning", "WELL"};
+            System.out.println(sentence.length);
+        }
+    }
+    //Returns 13
+
+
+//Q6
+
+    public class hw {
+        public static void main(String[] args);
+
+        String txt = "healthy", “speak”, “learning”, “king”, “Joy”, “Smile”, “Learn”, “live”, “give” , “living standard”, “life” ;
+
+	for(int i=0:i<11;i++){
+            If(words[i].strlen() > 5 &&  words[i].chatAt(0)=='l')
+
+            System.out.println(words[i]);
+        }
+
+    }
+
+}
